f6edb25dd1af82e33f55a44d9ca4e42b977865bd
Frvt5 <adrian.fryczkie@yahoo.com>
2024-01-17 12:32:52 +0100

    first commit

--- js/task-01.js
+++ js/task-01.js
@@ -10,3 +10,14 @@ console.log(`Elements: ${countElements}`)
 
 
 
+/*const titles = document.querySelectorAll("h2").length;
+console.log("Number of categories:", titles);
+
+const items = document.querySelectorAll("li.item");
+for (const item of items) {
+    const category = item.firstElementChild.textContent;
+    const elements = item.querySelectorAll("li").length;
+    console.log(`Category: ${category} \n Elements: ${elements}`);
+}
+
+*/
