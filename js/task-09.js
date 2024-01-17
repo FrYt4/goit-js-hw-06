ba4c3fa9925058406c07f9787a80a5924aa90b94
Frvt5 <adrian.fryczkie@yahoo.com>
2024-01-17 10:21:13 +0100

    first commit

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor(){
+  const body = document.body;
+  const colorSpan = document.querySelector(`.color`);
+  const randomColor = getRandomHexColor();
+
+   // Zmiana koloru tła elementu body
+  body.style.backgroundColor = randomColor;
+   // Wyświetlenie wartości koloru w span.color
+  colorSpan.textContent = randomColor;
+}
