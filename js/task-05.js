8c3fd5e58472d2961a22a2f0a9ff8e5386f6e6df
Frvt5 <adrian.fryczkie@yahoo.com>
2024-01-18 08:21:30 +0100

    first commit

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,8 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", (event) => {
- if(nameInput.ariaValueMax.trim === ""){
-    nameOutput.textContent = "Anonymous"
+ if(nameInput.value.trim === ""){
+    nameOutput.textContent = "Anonymous";
  }
  else{
     nameOutput.textContent = nameInput.value;
