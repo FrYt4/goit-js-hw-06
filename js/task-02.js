436ffa5e18ceac7c59db59dad27de3ed072e9c7d
Frvt5 <adrian.fryczkie@yahoo.com>
2024-01-19 07:36:05 +0100

    first commit

--- js/task-02.js
+++ js/task-02.js
@@ -22,7 +22,7 @@ const listItems = ingredients.map(ingredient => {
 const li = document.createElement(`li`);
 li.classList.add("item");
 li.textContent = ingredient
-allIngredients.append(...listItems);
+allIngredients.appendChild(li);
 console.log(li);
 
 return li;
